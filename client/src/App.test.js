140e57e12f799702f40df0b6a2cd8e1cbf2045fd
Vinni Hoke <vinni.hoke@gmail.com>
2019-09-15 22:22:07 -0700

    Attempted Various Tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,32 +1,49 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
 import App from "./App";
 import PlayerList from "./components/PlayerList";
-import { render, act } from "@testing-library/react";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Checking within App", () => {
+  test("App renders without crashing", () => {
+    rtl.render(<App />);
+  });
+
+  test("App contains specific Players", () => {
+    const wrapper = rtl.render(<App />);
+    // const player = wrapper.queryByText(/alex morgan/i);
+    // expect(player).toBeTruthy();
+    console.log(wrapper.debug());
+  });
+
+  test("App contains title", () => {
+    const wrapper = rtl.render(<App />);
+    const h1 = wrapper.getByText(/Womens World Cup Players/i);
+    expect(h1).toBeInTheDocument();
+  });
 });
 
-test("renders without crashing", () => {
-  //Arrange, Act, Assert
-  render(<App />);
+describe("Checking within PlayerList", () => {
+  test("Loading Fired?", () => {
+    const wrapper = rtl.render(<PlayerList />);
+    const element = wrapper.queryByText(/loading/i);
+    expect(element).toBeInTheDocument();
+  });
 });
 
-test("Check for Alex Morgan, Megan Rapinoe, and Carli Lloyd", () => {
-  const { getByText } = render(<PlayerList />);
-  getByText("Alex Morgan");
-});
+// test("Check for Alex Morgan, Megan Rapinoe, and Carli Lloyd", () => {
+//   const { getByText } = render(<PlayerList />);
+//   getByText("Alex Morgan");
+// });
 
-test("Test if something is present with TestID", () => {
-  // Give an element this data-testid="<id>"
-  const { getByTestId } = render(<App />);
-  getByTestId(/PlayerList/i);
-});
+// test("Test if something is present with TestID", () => {
+//   // Give an element this data-testid="<id>"
+//   const { getByTestId } = render(<App />);
+//   getByTestId(/PlayerList/i);
+// });
 
-test("Check if useFetch() worked", () => {
-  const { container } = render(<PlayerList />);
-  container.getByText("alex morgan");
-});
+// test("Check if useFetch() worked", () => {
+//   const { container } = render(<PlayerList />);
+//   container.getByText("alex morgan");
+// });

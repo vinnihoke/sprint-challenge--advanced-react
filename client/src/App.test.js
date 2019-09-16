8d998ea4f5b50031f40523ef597e1699e0a1d1d8
Vinni Hoke <vinni.hoke@gmail.com>
2019-09-15 21:09:34 -0700

    Stretch Completed

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { render } from "@testing-library/react";
-import * as rtl from "@testing-library/react";
-// import "jest-dom/extend-expect";
-
-afterEach(rtl.cleanup);
+import PlayerList from "./components/PlayerList";
+import { render, act } from "@testing-library/react";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
@@ -19,14 +16,17 @@ test("renders without crashing", () => {
 });
 
 test("Check for Alex Morgan, Megan Rapinoe, and Carli Lloyd", () => {
-  const { getByText } = render(<App />);
-  // IMPORTANT
-  // wrapper.getByText() returns either the node, or **FAILS THE TEST** outright:
-  expect(getByText(/Alex Morgan/i));
+  const { getByText } = render(<PlayerList />);
+  getByText("Alex Morgan");
 });
 
 test("Test if something is present with TestID", () => {
   // Give an element this data-testid="<id>"
   const { getByTestId } = render(<App />);
   getByTestId(/PlayerList/i);
+});
+
+test("Check if useFetch() worked", () => {
+  const { container } = render(<PlayerList />);
+  container.getByText("alex morgan");
 });
